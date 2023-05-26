aea7c8442cd98fa5fa46ed3f25329ae06c03b169
DrKapdor <drkapdor@gmail.com>
2023-05-26 23:04:25 +0300

    Добавьте файлы проекта.

---
+++ Algorithm/Bootstrap.cs
@@ -0,0 +1,46 @@
+﻿using System.Xml.Linq;
+
+namespace Algorithm
+{
+    public class Bootstrap
+    {
+        public static void Main(string[] args)
+        {
+            IndexedQueue<int> queue = new IndexedQueue<int>();
+            queue.Enqueue(5);
+            queue.Enqueue(7);
+            queue.Enqueue(2);
+            queue.Enqueue(453);
+            queue.Enqueue(23);
+            queue.Enqueue(1);
+            queue.Enqueue(1);
+            queue.Enqueue(78);
+            queue.Enqueue(34);
+            TestBinaryInsertionSort(queue);
+        }
+
+
+        private static void TestBinaryInsertionSort(IndexedQueue<int> queue)
+        {
+            queue.Comparator = new IntComparator();
+            long start = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            queue.BinaryInsertionSort();
+            long end = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            Console.WriteLine("Результат:");
+            while (queue.Count > 0)
+            {
+                Console.WriteLine(queue.Dequeue());
+            }
+            Console.WriteLine($"Время сортировки: {end - start} мс.");
+        }
+
+        //Создаём реализацию компаратора для целочисленных значений
+        private class IntComparator : IComparator<int>
+        {
+            public int Compare(int a, int b)
+            {
+                return a - b;
+            }
+        }
+    }
+}
